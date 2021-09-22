3187495de14c2e00e25144ab1a989636cd21d0c6
yyb-cr7 <77650911+yyb-cr7@users.noreply.github.com>
2021-09-22 15:00:10 +0800

    Finish the first function

--- test1.cpp
+++ test1.cpp
@@ -8,12 +8,22 @@ using namespace std;
 
 class statistics {
 	public:
-		int num = 0;
+		int count = 0;
 	
-	int count_kw(string file, char* key_words[]) {
-		cout << file <<endl;
-		return num;
+	int count_kw(string file, string key_words[]) {
+		// cout << file <<endl;
+		int begin = -1;
+		for (int i = 0; i < 32; i++) {
+			while((begin=file.find(key_words[i], begin+1)) != string::npos) {
+				count ++;
+				// cout << begin << "  " << count << endl;
+				begin = begin + key_words[i].length();
+			}	
+		}
+		return count;
 	}
+	
+	//int count_
 	//private:
 		
 		
@@ -23,14 +33,14 @@ int main()
 {
     //"E://test.txt"  "E:\\test_sf.c"
 	int stat [5] = {0,};
-	char* key_words[] ={"auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"};
-	ifstream ifile("E://test.txt");
+	string key_words[] = {"auto", "break", "case", "char", "const", "continue", "default", "do ", "double", "else", "enum", "extern", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"};
+	ifstream ifile("E://test1_sf.c");
 	ostringstream buf;
 	char ch;	
-	while(buf&&ifile.get(ch)) {
+	while(buf && ifile.get(ch)) {
 		buf.put(ch);
 	}
-	string str = buf.str();
+	string str = buf.str(); // transform the c file into string form
 	
 	statistics test;
 	stat[0] = test.count_kw(str, key_words);
