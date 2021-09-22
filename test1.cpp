1ab84ac153dca0fb76612a2bc058ab0c856c7cc6
yyb-cr7 <77650911+yyb-cr7@users.noreply.github.com>
2021-09-22 16:37:21 +0800

    finished the second function

--- test1.cpp
+++ test1.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 class statistics {
 	public:
-		int count = 0;
-	
+		int count, begin ; 
 	int count_kw(string file, string key_words[]) {
-		// cout << file <<endl;
-		int begin = -1;
+		//cout << file <<endl;
+		count = 0;  // all kinds of numbers will be initially sttled as 0
+		begin = -1;
 		for (int i = 0; i < 32; i++) {
 			while((begin=file.find(key_words[i], begin+1)) != string::npos) {
 				count ++;
@@ -23,7 +23,30 @@ class statistics {
 		return count;
 	}
 	
-	//int count_
+	int count_sw(string file) {
+		count = 0;
+		begin = -1;
+		while((begin=file.find("switch", begin+1)) != string::npos) {
+				count ++;
+				//cout << begin << "  " << count << endl;
+				begin = begin + 5;
+			}
+		return count;
+	} 
+	
+	int* count_ca(int sw_num, string file) {
+		int* count = new int [sw_num];
+		count[0] = 0, count[1] = 0; 
+		begin = -1;
+		for(int i = 0; i < sw_num; i++) {
+			while((begin=file.find("case", begin+1)) != string::npos) {
+				count ++;
+				//cout << begin << "  " << count << endl;
+				begin = begin + 4;
+				}
+			return count;
+		}
+	} 
 	//private:
 		
 		
@@ -44,6 +67,10 @@ int main()
 	
 	statistics test;
 	stat[0] = test.count_kw(str, key_words);
+	stat[1] = test.count_sw(str);
+	int sw_num = stat[1];
+	//stat[2] = test.count_ca(sw_num, str);
+	//cout << stat[0] << endl;
 	
 	cout << "total num: " << stat[0] << endl;
 	cout << "switch num: " << stat[1] << endl;
